8e619eb992409eeaf9f6ffa36ca28a1bc6c34ac0
Seruf <alexche92@gmail.com>
2019-12-27 12:11:27 +0200

    Updates

---
+++ precache-manifest.96531eaa8accf0f45a36a4a452926485.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b8858283e7647662ed13c3db308ec1",
+    "url": "/EasyCode-Final-Project/index.html"
+  },
+  {
+    "revision": "cfce77745b2219ac1a36",
+    "url": "/EasyCode-Final-Project/static/css/main.64244df2.chunk.css"
+  },
+  {
+    "revision": "d669d9e3eff20118f007",
+    "url": "/EasyCode-Final-Project/static/js/2.40fbc01f.chunk.js"
+  },
+  {
+    "revision": "cfce77745b2219ac1a36",
+    "url": "/EasyCode-Final-Project/static/js/main.e243290c.chunk.js"
+  },
+  {
+    "revision": "eab41aa7b445fc71b41e",
+    "url": "/EasyCode-Final-Project/static/js/runtime-main.afb54eab.js"
+  },
+  {
+    "revision": "946d38c8b0602beac3633cbbc500e759",
+    "url": "/EasyCode-Final-Project/static/media/1.946d38c8.jpg"
+  },
+  {
+    "revision": "c419d76532139c6373ba5e101c429881",
+    "url": "/EasyCode-Final-Project/static/media/2.c419d765.jpg"
+  },
+  {
+    "revision": "40d734332a0d51477b6a9b49d8810961",
+    "url": "/EasyCode-Final-Project/static/media/3.40d73433.jpg"
+  },
+  {
+    "revision": "77b09f8b5e453f88da1fccac330fa88f",
+    "url": "/EasyCode-Final-Project/static/media/4.77b09f8b.jpg"
+  },
+  {
+    "revision": "6e5f6ca4be569c42889e3554a6e888a5",
+    "url": "/EasyCode-Final-Project/static/media/404travolta.6e5f6ca4.gif"
+  },
+  {
+    "revision": "b67f132404e34153bb3e3b9977fb33e7",
+    "url": "/EasyCode-Final-Project/static/media/accessories.b67f1324.png"
+  },
+  {
+    "revision": "1e7cb0fce264e036c7df5cad38113b8e",
+    "url": "/EasyCode-Final-Project/static/media/applewatch.1e7cb0fc.png"
+  },
+  {
+    "revision": "fe57e0f6fa111a173f71f924d0ddd941",
+    "url": "/EasyCode-Final-Project/static/media/imac.fe57e0f6.png"
+  },
+  {
+    "revision": "6b74be9e2f995f8e8e4a2f6ff0225276",
+    "url": "/EasyCode-Final-Project/static/media/ipad.6b74be9e.jpg"
+  },
+  {
+    "revision": "500f19be5c6426ff6fe684900771032b",
+    "url": "/EasyCode-Final-Project/static/media/iphone.500f19be.jpeg"
+  },
+  {
+    "revision": "71e7f38a5899dd2ee5d618b29f4679c4",
+    "url": "/EasyCode-Final-Project/static/media/logo-x2.71e7f38a.png"
+  },
+  {
+    "revision": "0515c5b773298b8faf6066dd1becc01d",
+    "url": "/EasyCode-Final-Project/static/media/logo.0515c5b7.png"
+  },
+  {
+    "revision": "9139f7789a05f198c900b37eaa1aafab",
+    "url": "/EasyCode-Final-Project/static/media/macbook.9139f778.png"
+  }
+]);
